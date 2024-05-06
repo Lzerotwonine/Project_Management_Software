50f127fb680afe1a2004e5f5f85820eab5146a2c
Lzerotwonine <125208234+Lzerotwonine@users.noreply.github.com>
2024-05-06 11:19:28 +0700

    Update createDB.py

--- createDB.py
+++ createDB.py
@@ -2,17 +2,20 @@ from pymongo import MongoClient
 from datetime import datetime
 import random
 
-def connect_to_mongodb():
+def connect_to_mongodb(collection_name):
     client = MongoClient('mongodb://localhost:27017/')
     db = client['QuanLyDuAnPhanMem']
-    collection = db['SanPham']
+    collection = db[collection_name]
     return collection
 
 # Danh sách các trạng thái
 statuses = ["Đã giao", "Đang vận chuyển", "Đã xác nhận"]
 
-# Kết nối đến cơ sở dữ liệu MongoDB
-collection = connect_to_mongodb()
+# Khi bạn muốn kết nối đến collection 'TaiKhoan':
+account = connect_to_mongodb('TaiKhoan')
+
+# Khi bạn muốn kết nối đến collection 'SanPham':
+collection = connect_to_mongodb('SanPham')
 
 # Tạo và thêm 10 đơn hàng với trạng thái ngẫu nhiên
 for i in range(10, 21):
